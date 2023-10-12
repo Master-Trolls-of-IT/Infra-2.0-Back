220446f06ab723e4b34a89a5a8848775734b336b
AkaMirai9 <tom.frenois@epita.fr>
2023-10-12 16:14:53 +0200

    ajout create

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var cookieParser = require('cookie-parser');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var query = require('../db/query').query
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
   try {
     res.json(await query('SELECT * FROM users;'))
   } catch (e) {
@@ -11,5 +11,14 @@ router.get('/', async function (req, res, next) {
   }
 
 });
+
+router.post('/', async function (req, res) {
+  try {
+    await query(`INSERT INTO users(username, lastname, firstname, age, rights, status) VALUES (${req.body.username}, ${req.body.lastname}, ${req.body.firstname}, ${req.body.age}, ${req.body.rights}, ${req.body.status})`)
+    res.status(201).json({message: 'Utilisateur créé avec succès', user: req.body})
+  } catch (e) {
+    res.status(500).json({error: e})
+  }
+})
 
 module.exports = router;
