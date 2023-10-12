db350d0b88d5b9aa49ec1091d3c8077c2a29bb5b
AkaMirai9 <tom.frenois@epita.fr>
2023-10-12 14:49:04 +0200

    Ajout de la route get user

---
+++ db/config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    host: 'ec2-13-39-155-15.eu-west-3.compute.amazonaws.com',
+    user: 'back',
+    password: '1234',
+    database: 'gaia_admin'
+  },
+  listPerPage: 10,
+};
+module.exports = config;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../db/query').query
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+  try {
+    res.json(await query('SELECT * FROM users;'))
+  } catch (e) {
+    res.status(500).json({error: e})
+  }
+
 });
 
 module.exports = router;
