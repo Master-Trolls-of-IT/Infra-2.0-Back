7dfa0768e40ff013aa2dcf245d52f3cae0d0ea26
AkaMirai9 <105291924+AkaMirai9@users.noreply.github.com>
2024-01-10 22:48:27 +0100

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const config = {
   db: {
-    host: 'ec2-13-39-109-144.eu-west-3.compute.amazonaws.com',
+    host: process.env.URL_DB,
     user: 'back',
     password: '#1234Abc',
     database: 'gaia_admin'
